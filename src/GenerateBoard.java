5f8bbaee5cb22919204bedf3917599ff8603fab2
Jason Odell <73271208+jasonodell2001@users.noreply.github.com>
2022-02-24 11:48:16 -0500

    added board generation Integer Size determines the size of the board being Size X Size Currently generates a random number and checks if it is divisible by 7 to determine if it should be a terminal, formula can be tweaked

--- src/GenerateBoard.java
+++ src/GenerateBoard.java
@@ -1,2 +1,41 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.*;
 public class GenerateBoard {
+    public static void main(String[] args) throws IOException {
+        genBoard(5);
+    }
+    public static void genBoard(int size) throws IOException {
+        Writer fileWriter = new FileWriter("board.txt");
+        int boardSize = size;
+        Random rng = new Random();
+        ArrayList<ArrayList<String>> board = new ArrayList<>();
+        for(int i = 0; i < boardSize; i++){
+            ArrayList<String> row = new ArrayList<>();
+            if(rng.nextInt() % 7 == 0){
+                row.add(""+rng.nextInt(-9,10));
+            }
+            else{
+                row.add("" + 0);
+            }
+            for(int j = 1; j < boardSize; j++){
+                if(rng.nextInt() % 7 == 0) {
+                    row.add("\t" + rng.nextInt(-9, 10));
+                }
+                else{
+                    row.add("\t"+ 0);
+                }
+            }
+            board.add(row);
+        }
+
+        for(int i = 0; i < boardSize; i++){
+            for(int j = 0; j <boardSize; j++){
+                fileWriter.write(board.get(i).get(j));
+            }
+            fileWriter.write("\n");
+        }
+        fileWriter.close();
+    }
 }
